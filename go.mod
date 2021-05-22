934d126330afd089f1cfd29fe065fdf8e00c1dcf
floxydio <diooktar@gmail.com>
2021-05-22 22:22:50 +0700

    Ganti

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module belajargo
 go 1.16
 
 require (
+	github.com/go-playground/validator v9.31.0+incompatible // indirect
+	github.com/go-playground/validator/v10 v10.6.1 // indirect
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16
+	github.com/thedevsaddam/govalidator v1.9.10 // indirect
 )
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/go-playground/validator"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 
@@ -17,16 +18,14 @@ var db *gorm.DB
 var err error
 
 type Product struct {
-	Id    uint   `json:"id" gorm:"primary_key"`
-	Title string `json:"title"`
-	Price int    `json:"harga"`
+	Id     uint   `form:"id" json:"id" gorm:"primaryKey" `
+	Title  string `form:"title" json:"title"`
+	Price  int    `form:"harga" json:"harga"`
+	Author string `form:"author" json:"author"`
 }
 
-type User struct {
-	Id       uint   `json:"id" gorm: "primary_key"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Level    int    `json:"level"`
+type CustomValidator struct {
+	validator *validator.Validate
 }
 
 type Result struct {
@@ -35,29 +34,51 @@ type Result struct {
 	Message string      `json:"message"`
 }
 
+var validate *validator.Validate
+
 func main() {
+
 	db, err = gorm.Open("mysql", "root:@tcp(localhost)/belajargo")
+	validate = validator.New()
 	if err != nil {
 		log.Println("Connection failed", err)
 	} else {
-		log.Println("Connection Connect", err)
+		log.Println("Connection Connect")
 	}
 
 	db.AutoMigrate(&Product{})
-	db.AutoMigrate(&User{})
 
 	handleRequest()
 
 }
 
 func handleRequest() {
+
 	log.Println("Start at 8000")
 
 	myRouter := mux.NewRouter().StrictSlash(true)
 
+	myRouter.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+
+		res := Result{Code: 404, Message: "Method not found"}
+		response, _ := json.Marshal(res)
+		w.Write(response)
+	})
+
+	myRouter.MethodNotAllowedHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+
+		res := Result{Code: 403, Message: "Method not allowed"}
+		response, _ := json.Marshal(res)
+		w.Write(response)
+	})
+
 	myRouter.HandleFunc("/", homepage)
 	myRouter.HandleFunc("/api/products", products).Methods("POST")
-	myRouter.HandleFunc("/api/register", register).Methods("POST")
+	myRouter.HandleFunc("/api/products", getProducts).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8000", myRouter))
 }
@@ -71,19 +92,27 @@ func products(w http.ResponseWriter, r *http.Request) {
 
 	var product Product
 	json.Unmarshal(payloads, &product)
+	db.Create(&product)
 
-	db.Create(product)
+	res := Result{Code: 200, Data: product, Message: "Berhasil Menambahkan Produk"}
+	result, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+
 }
 
-func register(w http.ResponseWriter, r *http.Request) {
-	reqBody, _ := ioutil.ReadAll(r.Body)
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	products := []Product{}
 
-	var user User
-	json.Unmarshal(reqBody, &user)
+	db.Find(&products)
 
-	db.Create(&user)
-
-	res := Result{Code: 200, Data: user, Message: "Berhasil Registrasi"}
+	res := Result{Code: 200, Data: products, Message: "Products Get"}
 	result, err := json.Marshal(res)
 
 	if err != nil {
@@ -94,4 +123,8 @@ func register(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(result)
 
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	return cv.validator.Struct(i)
 }
